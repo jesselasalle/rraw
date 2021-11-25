57cc354a31e821ee0de914468926a07888ce1fb6
Jesse LaSalle <jesselasalle@gmail.com>
2021-11-25 15:31:16 -0500

    Initial commit

---
+++ lib/rraw.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rraw/version"
+
+module Rraw
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/rraw/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Rraw
+  VERSION = "0.1.0"
+end
